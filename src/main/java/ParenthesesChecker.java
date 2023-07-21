bdda41b733181a6c8da5c75405e5db90dea277d3
sarahoco014 <soconnor2@outlook.com>
2023-07-21 12:43:38 +0100

    adds solution

--- src/main/java/ParenthesesChecker.java
+++ src/main/java/ParenthesesChecker.java
@@ -1,18 +1,42 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class ParenthesesChecker {
 
-    public boolean checkParentheses(String test) {
-        if(test.isEmpty()) {
-            return true;
-        } else if (test == "()") {
-            return true;
+    private Stack<Character> unmatchedCharacters;
 
-        } else if(test == "(") {
-            return false;
-        } else if(test == ")") {
-            return false;
-        } else if(test == "(()") {
-            return false;
-        }
-        return false;
+    public ParenthesesChecker(){
+        unmatchedCharacters = new Stack<>();
     }
+
+    public boolean checkParentheses(String testString) {
+
+        List<Character> openingCharacters = Arrays.asList('(', '{', '[', '<');
+        List<Character> closingCharacters = Arrays.asList(')', '}', ']', '>');
+
+        char[] brokenString = testString.toCharArray();
+
+        for (Character character : brokenString){
+            if (openingCharacters.contains(character)){
+                unmatchedCharacters.push(character);
+            }
+            else if (closingCharacters.contains(character)){
+
+                int indexToCheck = closingCharacters.indexOf(character);
+
+                char correspondingOpeningCharacter = openingCharacters.get(indexToCheck);
+
+                if (unmatchedCharacters.isEmpty() || !unmatchedCharacters.peek().equals(correspondingOpeningCharacter)) {
+                    return false;
+                }
+                else {
+                    unmatchedCharacters.pop();
+                }
+            }
+        }
+
+        return unmatchedCharacters.isEmpty();
+    }
+
 }
